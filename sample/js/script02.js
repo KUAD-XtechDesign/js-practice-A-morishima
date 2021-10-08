f3183d479277caf656742c9845d9deb87ffd9d27
A-morishima <a-morishima3277@st.kyoto-art.ac.jp>
2021-10-08 16:12:27 +0900

    サンプル修正

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -2,27 +2,28 @@ $(function(){
 
     let txt1 = "おはようございます";
     let txt2 = "おげんきですか";
-    let num1 = 19800;
-    let num2 = 1.1;
+    let num1 = 20;
+    let num2 = 32;
 
     $("#button01").on("click",function(){
-        $("content").text("こんにちは!");
+        $("#content").text("こんにちは!");
     })
 
     $("#button02").on("click",function(){
-        $("content").text(txt1);
+        $("#content").text(txt1);
     })
 
     $("#button03").on("click",function(){
-        $("content").text(txt1 + txt2);
+        $("#content").text(txt1 + txt2);
     })
 
 $("#button04").on("click",function(){
-        $("content").text(num1 + num2);
+        $("#content").text(num1 + num2);
     })
     
     $("#button05").on("click",function(){
-        $("content").text(num1+ "たす" + num2 + "は"　+ (num1 +num2) + "です");
+        $("#content").text(num1+ "たす" + num2 + "は"　+ (num1 +num2) + "です");
     })
+
     
 });
